c1b466dc0cd4e9d83343c6cb88ff6835fc620ace
MagdyIsmail <Magdy.Ismail@medafinvestment.com>
2019-07-07 19:05:18 +0200

    #

--- CarVendor.data/Entities/CarColor.cs
+++ CarVendor.data/Entities/CarColor.cs
@@ -7,6 +7,7 @@ namespace CarVendor.data.Entities
         public long CarId { get; set; }
         public long ColorId { get; set; }
         public virtual ICollection<CarImage> CarImages { get; set; }
+
         public virtual Car Car { get; set; }
         public virtual Color Color { get; set; }
     }
