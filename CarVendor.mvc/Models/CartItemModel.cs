42c8e6eef7bf53884769e0f7e62b66bbb2575f36
Islam Gad <i_gad_@hotmail.com>
2019-07-09 10:54:21 +0200

    Add to cart and redirect to cart page

---
+++ CarVendor.mvc/Models/CartItemModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarVendor.mvc.Models
+{
+    public class CartItemModel
+    {
+        public long CarId { get; set; }
+        public ColorModel Color { get; set; }
+        public CategoryModel Category { get; set; }
+        public decimal  Price { get; set; }
+        public long Quantity { get; set; }
+    }
+}
