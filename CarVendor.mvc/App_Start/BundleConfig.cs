1ffde67aa95ba71d235df8328fdf86679f8e941f
MagdyIsmail <Magdy.Ismail@medafinvestment.com>
2019-07-28 13:42:57 +0200

    #

--- CarVendor.mvc/App_Start/BundleConfig.cs
+++ CarVendor.mvc/App_Start/BundleConfig.cs
@@ -5,22 +5,12 @@ namespace CarVendor.mvc
 {
     public class BundleConfig
     {
+  
+
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Content/Scripts/jquery-{version}.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Content/Scripts/jquery.validate*"));
-
-            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
-            //// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Content/Scripts/modernizr-*"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js"));
+            BundleTable.EnableOptimizations = true;
 
             bundles.Add(new ScriptBundle("~/Content/js").Include(
                 "~/Content/js/jquery.min.js",
@@ -33,9 +23,6 @@ namespace CarVendor.mvc
                 "~/Content/js/jquery.tabs.js",
                 "~/Content/js/wow/wow.js",
                 "~/Content/js/bootstrap.min.js"));
-
-
-
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/site.css",
