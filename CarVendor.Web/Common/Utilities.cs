e9ae6b9f731e1a80a18bb531b3785365edb09072
MahmoudOomar <mahmoud.omar@medafinvestment.com>
2019-11-18 16:31:03 +0200

    Excel sheet and order date

--- CarVendor.Web/Common/Utilities.cs
+++ CarVendor.Web/Common/Utilities.cs
@@ -5,22 +5,24 @@ using CarVendor.Web.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
 namespace CarVendor.mvc.Common
 {
     public class Utilities
     {
         public static List<CartModel> _shopingCarts = new List<CartModel>();
-        public static CurrencyDTO _currencyDTO = new CurrencyDTO() { Code=1,Name="EGP"};
-    
-      
-        public static long SetOrderDetails( DataBaseContext db, CreditCardModel creditCard =null, BankTransferModel BankTransfer=null,long UserId=0)
+        public static CurrencyDTO _currencyDTO = new CurrencyDTO() { Code = 1, Name = "EGP" };
+
+
+        public static long SetOrderDetails(DataBaseContext db, CreditCardModel creditCard = null, BankTransferModel BankTransfer = null, long UserId = 0)
         {
 
             var customer_cart = _shopingCarts.FirstOrDefault();
             if (customer_cart == null && customer_cart.CustomerInfo == null && customer_cart.CartItems == null && customer_cart.CartItems.Count < 1)
+            {
                 return -1;
+            }
+
             List<OrderItem> newOrderItems = new List<OrderItem>();
             foreach (var orderItem in customer_cart.CartItems)
             {
@@ -30,21 +32,21 @@ namespace CarVendor.mvc.Common
                     Color = orderItem.Color.Text,
                     Quantity = (int)orderItem.Quantity,
                     Category = orderItem.Category.Text,
-                    IsDeleted=false
-                    
+                    IsDeleted = false
+
                 });
             }
 
             Order newOrder = new Order()
             {
                 OrderDate = DateTime.Now,
-                OrderNumber ="S"+ DateTime.Now.Day.ToString() + "I" + DateTime.Now.Month + "G" + DateTime.Now.Year.ToString().Substring(2,2),
+                OrderNumber = "S" + DateTime.Now.Day.ToString() + "I" + DateTime.Now.Month + "G" + DateTime.Now.Year.ToString().Substring(2, 2),
                 OrderItems = newOrderItems,
-                IsDeleted=false,
-                UserId= UserId
+                IsDeleted = false,
+                UserId = UserId
             };
             customer_cart.Order = newOrder;
-             CardInfo card = null;
+            CardInfo card = null;
             if (creditCard != null)
             {
                 card = new CardInfo()
@@ -100,7 +102,7 @@ namespace CarVendor.mvc.Common
                 MName = u.MName,
                 LName = u.LName,
                 Phone = u.Phone,
-                DeliveryAddress = u.UserAddresses.Where(a=>a.User.Id==UserId).FirstOrDefault().Address.DeliveryAddress,
+                DeliveryAddress = u.UserAddresses.Where(a => a.User.Id == UserId).FirstOrDefault().Address.DeliveryAddress,
                 MainAddress = u.UserAddresses.Where(a => a.User.Id == UserId).FirstOrDefault().Address.MainAddress
 
                 //DeliveryAddress=u.UserAddresses,
@@ -110,22 +112,22 @@ namespace CarVendor.mvc.Common
             EmailTemplate Email = new EmailTemplate();
             string path = @"~/Common/OrderDetailsEmailTemplate.html";
             var emailHtml = Email.ReadTemplateEmail(customer_cart, path);
-            GmailSender.SendEmail("mpay.services@medafinvestment.com", "Serious!1", db.Mails.Select(s=>s.mail).ToList() , "Order", emailHtml, null);
+            GmailSender.SendEmail("mpay.services@medafinvestment.com", "Serious!1", db.Mails.Select(s => s.mail).ToList(), "Order", emailHtml, null);
             try
             {
                 db.SaveChanges();
             }
             catch (Exception ex)
             {
-             
-                throw new Exception("error",ex);
+
+                throw new Exception("error", ex);
 
             }
-        
+
 
             return newOrder.Id;
-            
+
         }
 
-          }
+    }
 }
